10bde760b97cb3073f7a89444ab3d12cd232af48
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-07-25 09:55:38 -0700

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+
+
+/**
+ * lomuto_partition - order a subset of an array of integers according to 
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ * Return: The final partition index.
+ */
+int lomuto_partitions(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below, temp;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				temp = array[below];
+				array[below] = array[above];
+				array[above] = temp;
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		temp = array[below];
+		array[below] = array[above];
+		array[above] = temp;
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * lomuto_sort - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partitions(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of integers in ascending
+ * @array: An array of integers.
+ * @size: The size of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	lomuto_sort(array, size, 0, size - 1);
+}
